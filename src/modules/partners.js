b96f84bb0842e661eb9d9a61ccfb95aea88a1b3c
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2021-11-07 18:04:30 +0300

    feat: add webpack

---
+++ src/modules/partners.js
@@ -0,0 +1,16 @@
+const partners = () => {
+    const renderItems = (data) => {
+        console.log(data);
+    }
+    
+    fetch('https://delivery-food-61298-default-rtdb.firebaseio.com/db/partners.json')
+        .then((response) => response.json())
+        .then((data) => {
+            renderItems(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+export default partners
